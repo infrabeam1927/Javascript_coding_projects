3403e79aad11f89cb1610484e8c6febaafc17d9a
Aaditya Senthilkumar <75241997+infrabeam1927@users.noreply.github.com>
2023-03-10 10:39:17 -0500

    Add files via upload

---
+++ 01basics/variables.js
@@ -0,0 +1,10 @@
+let name = 'Aadi'
+const yo='aaddddooooooiiiiii'
+let score = 100
+let bonus = 20
+let totalscore=score+bonus
+console.log(totalscore)
+
+let firstName= 'Aadi'
+let lastName='Senthilkumar'
+
